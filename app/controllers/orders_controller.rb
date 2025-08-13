152c846d1fd1706bde6805e59d81fb20cb8339d3
fukusilyuu <ichigosister@icloud.com>
2025-08-14 08:31:57 +0900

    formオブジェクトパターン

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,28 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:index, :create]
+
+  def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @order = OrderAddress.new
+  end
+
+  def create
+    @order = OrderAddress.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render 'index', status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_order
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
