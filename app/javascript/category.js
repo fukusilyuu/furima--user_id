e1e0f8274619572a88c8999004a2559244f94b3a
fukusilyuu <ichigosister@icloud.com>
2025-09-18 02:13:51 +0900

    css

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -3,10 +3,16 @@ window.addEventListener('load', function(){
   const Categoryslists = document.getElementById('lists')
   const Categoryslist = document.querySelector('list')
   const Categorysprice = document.getElementById('price')
-  //Categoryslists.style.display = 'none';
-  Categorys.addEventListener('click', function() {
-    Categoryslists.getAttribute("style","display:block;")
+  const del = document.getElementById("category-del");
 
+  Categoryslists.style.display = 'none';
+  Categorys.addEventListener('click', function() {
+    del.removeAttribute("style");
+    if (Categoryslists.getAttribute("style") == "display:block;") {
+      Categoryslists.setAttribute("style");
+    } else {
+      Categoryslists.setAttribute("style", "display:block;");
+    };
      // cre
   });
  
