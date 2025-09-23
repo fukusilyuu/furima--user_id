c5069f7430239b7a27263fd54244641a8c0cd112
fukusilyuu <ichigosister@icloud.com>
2025-09-24 08:47:40 +0900

    修正css

---
+++ app/javascript/comment.js
@@ -0,0 +1,20 @@
+const comment = () => {
+  const CommentRan = document.getElementById("comment-ran");
+  const Comments = document.getElementById("comments")
+  CommentRan.addEventListener('click',function() {
+    if (Comments.getAttribute("style") == "display:block;") {
+      Comments.setAttribute("style");
+    } else {
+      Comments.setAttribute("style", "display:block;");
+    };
+    Comments.addEventListener('click', function() {
+      if (Comments.getAttribute("style") != "display:block;") {
+        Comments.setAttribute("style", "display:block;");
+      } else {
+        Comments.setAttribute("style");
+      };
+    })
+  })
+};
+window.addEventListener("turbo:load", comment);
+window.addEventListener("turbo:render", comment);
