6e7b45a1cfd0968df5ad5f506e4f96bffd7e4d20
fukusilyuu <ichigosister@icloud.com>
2025-09-26 01:31:19 +0900

    javasript

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -2,17 +2,9 @@ const comment = () => {
   const CommentRan = document.getElementById("comment-ran");
   const Comments = document.getElementById("comments")
   CommentRan.addEventListener('click',function() {
-    if (Comments.getAttribute("style") == "display:block;") {
-      Comments.setAttribute("style");
-    } else {
-      Comments.setAttribute("style", "display:block;");
-    };
-    Comments.addEventListener('click', function() {
-      if (Comments.getAttribute("style") == "display:block;") {
-        Comments.setAttribute("style", "display:block;");
-      } else {
-        Comments.setAttribute("style");
-      };
+    Comments.setAttribute("style", "display:block;");
+    CommentRan.addEventListener('click', function() {
+      Comments.setAttribute("style", "display:none;");
     })
   })
 };
